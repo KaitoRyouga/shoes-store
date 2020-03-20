21c538a81f58153c2f7ffcd1a31447b213a3533d
huuluantvka2 <57451218+huuluantvka2@users.noreply.github.com>
2020-03-20 22:28:34 +0700

    hi

--- js/script.js
+++ js/script.js
@@ -8,4 +8,36 @@ $(document).ready(function () {
          $('.navbar-expand-lg').addClass('bg-transparent').addClass('navbar-light').removeClass('bd--dark').removeClass('navbar-light');
       }
     });
+    $('.sub-title').addClass('sub-title-go-to-down').one('webkitAnimationEnd', function(event) {
+    	$('.sub-title').removeClass('sub-title-go-to-down');
+    });
+	$('.main-title').addClass('main-title-go-to-left').one('webkitAnimationEnd', function(event) {
+		$('.main-title').removeClass('main-title-go-to-left')	
+	});
+    $('#next').click(function(event) {
+    	var slide_sau = $('.active-slide').next();
+    	if(slide_sau != 0){
+	    	$('.active-slide').removeClass('active-slide');
+	    	slide_sau.addClass('active-slide');
+	  //   	$('.sub-title').addClass('sub-title-go-to-down').one('webkitAnimationEnd', function(event) {
+   //  			$('.sub-title').removeClass('sub-title-go-to-down');
+   //  		});
+			// $('.main-title').addClass('main-title-go-to-left').one('webkitAnimationEnd', function(event) {
+			// 	$('.main-title').removeClass('main-title-go-to-left')	
+			// });
+
+	    }
+	    else{
+	    	$('.active-slide').removeClass('active-slide');
+	    	$('#slide-first').addClass('active-slide');
+	  //   	$('.active-slide').removeClass('active-slide');
+	  //   	$('.slide-position').addClass('active-slide');
+	  //   	$('.sub-title').addClass('sub-title-go-to-down').one('webkitAnimationEnd', function(event) {
+   //  			$('.sub-title').removeClass('sub-title-go-to-down');
+   //  		});
+			// $('.main-title').addClass('main-title-go-to-left').one('webkitAnimationEnd', function(event) {
+			// 	$('.main-title').removeClass('main-title-go-to-left')	
+			// });
+	    }
+	});
 });
