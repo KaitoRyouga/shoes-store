a39a419a9a759c657f4b8b050a82ca268451d791
huuluantvka2 <57451218+huuluantvka2@users.noreply.github.com>
2020-03-21 22:32:13 +0700

    up nha

--- js/script.js
+++ js/script.js
@@ -8,36 +8,55 @@ $(document).ready(function () {
          $('.navbar-expand-lg').addClass('bg-transparent').addClass('navbar-light').removeClass('bd--dark').removeClass('navbar-light');
       }
     });
-    $('.sub-title').addClass('sub-title-go-to-down').one('webkitAnimationEnd', function(event) {
+    $('.active-slide').find('.sub-title').addClass('sub-title-go-to-down').one('webkitAnimationEnd', function(event) {
     	$('.sub-title').removeClass('sub-title-go-to-down');
     });
-	$('.main-title').addClass('main-title-go-to-left').one('webkitAnimationEnd', function(event) {
-		$('.main-title').removeClass('main-title-go-to-left')	
-	});
+    $('.active-slide').find('.main-title').addClass('main-title-go-to-left').one('webkitAnimationEnd', function(event) {
+    	$('.main-title').removeClass('main-title-go-to-left');
+    });
     $('#next').click(function(event) {
     	var slide_sau = $('.active-slide').next();
-    	if(slide_sau != 0){
-	    	$('.active-slide').removeClass('active-slide');
-	    	slide_sau.addClass('active-slide');
-	  //   	$('.sub-title').addClass('sub-title-go-to-down').one('webkitAnimationEnd', function(event) {
-   //  			$('.sub-title').removeClass('sub-title-go-to-down');
-   //  		});
-			// $('.main-title').addClass('main-title-go-to-left').one('webkitAnimationEnd', function(event) {
-			// 	$('.main-title').removeClass('main-title-go-to-left')	
-			// });
-
-	    }
-	    else{
-	    	$('.active-slide').removeClass('active-slide');
-	    	$('#slide-first').addClass('active-slide');
-	  //   	$('.active-slide').removeClass('active-slide');
-	  //   	$('.slide-position').addClass('active-slide');
-	  //   	$('.sub-title').addClass('sub-title-go-to-down').one('webkitAnimationEnd', function(event) {
-   //  			$('.sub-title').removeClass('sub-title-go-to-down');
-   //  		});
-			// $('.main-title').addClass('main-title-go-to-left').one('webkitAnimationEnd', function(event) {
-			// 	$('.main-title').removeClass('main-title-go-to-left')	
-			// });
-	    }
-	});
+    	if(slide_sau.length != 0){
+    		$('.active-slide').removeClass('active-slide');
+    		slide_sau.addClass('active-slide');
+    		$('.active-slide').find('.sub-title').addClass('sub-title-go-to-down').one('webkitAnimationEnd', function(event) {
+    			$('.sub-title').removeClass('sub-title-go-to-down');
+    		});
+    		$('.active-slide').find('.main-title').addClass('main-title-go-to-left').one('webkitAnimationEnd', function(event) {
+    			$('.main-title').removeClass('main-title-go-to-left');
+    		});
+    	}else{
+    		$('.active-slide').removeClass('active-slide');
+    		$('.slide-position:first').addClass('active-slide');
+    		$('.active-slide').find('.sub-title').addClass('sub-title-go-to-down').one('webkitAnimationEnd', function(event) {
+    			$('.sub-title').removeClass('sub-title-go-to-down');
+    		});
+    		$('.active-slide').find('.main-title').addClass('main-title-go-to-left').one('webkitAnimationEnd', function(event) {
+    			$('.main-title').removeClass('main-title-go-to-left');
+    		});
+    	}
+    });
+    $('#prev').click(function(event) {
+    	var slide_truoc = $('.active-slide').prev();
+    	if(slide_truoc.length != 0){
+    		$('.active-slide').removeClass('active-slide');
+    		slide_truoc.addClass('active-slide');
+    		$('.active-slide').find('.sub-title').addClass('sub-title-go-to-down').one('webkitAnimationEnd', function(event) {
+    			$('.sub-title').removeClass('sub-title-go-to-down');
+    		});
+    		$('.active-slide').find('.main-title').addClass('main-title-go-to-left').one('webkitAnimationEnd', function(event) {
+    			$('.main-title').removeClass('main-title-go-to-left');
+    		});
+    	}
+    	else{
+    		$('.active-slide').removeClass('active-slide');
+    		$('.slide-position:last').addClass('active-slide');
+    		$('.active-slide').find('.sub-title').addClass('sub-title-go-to-down').one('webkitAnimationEnd', function(event) {
+    			$('.sub-title').removeClass('sub-title-go-to-down');
+    		});
+    		$('.active-slide').find('.main-title').addClass('main-title-go-to-left').one('webkitAnimationEnd', function(event) {
+    			$('.main-title').removeClass('main-title-go-to-left');
+    		});
+    	}
+    });
 });
